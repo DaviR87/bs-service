406f1104f7b084b184130d182c0afcd5d58ffb99
Davide <DaviR87@users.noreply.github.com>
2022-10-27 11:05:12 +0200

    Added project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RightComponent } from './right/right.component';
+import { LeftComponent } from './left/left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RightComponent,
+    LeftComponent
   ],
   imports: [
     BrowserModule
